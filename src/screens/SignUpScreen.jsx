54a2990929ab9322408ae95331e2080929b54188
yumu-engineer <yumu.engineer@gmail.com>
2021-05-06 19:05:35 +0900

    create LogInScreen and SignUpScreen

---
+++ src/screens/SignUpScreen.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  View, Text, TextInput, StyleSheet,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import Button from '../components/Button';
+
+export default function SignUpScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.inner}>
+        <Text style={styles.title}>Sign Up</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="Password" />
+        <Button label="Submit" />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Already registered?</Text>
+          <Text style={styles.footerLink}>Log In.</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f4f8',
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    height: 48,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+    borderWidth: 1,
+    borderColor: '#dddddd',
+    backgroundColor: '#ffffff',
+    fontSize: 16,
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467fd3',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+});
